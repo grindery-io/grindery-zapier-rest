52a25a09dc366a00f57240770eebc48ec85460ad
Connex Digital <clientaccess@connex.digital>
2022-10-25 13:10:39 -0400

    request body

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post("/webhooks", async (req, res) => {
   client.connect(async (err) => {
     const collection = client.db("grindery_zapier").collection("webhooks");
     // perform actions on the collection object
-    console.log(req.body); //DEBUG: Logging
+    console.log("Request Body", req.body); //DEBUG: Logging
 
     const hook_url = req.body.url;
     const hook_token = req.body.token;
