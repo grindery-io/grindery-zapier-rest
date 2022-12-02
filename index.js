5730097ebfc6b79fecdbd5de31f3890df03d6109
Connex Digital <clientaccess@connex.digital>
2022-12-02 02:11:36 -0400

    log

--- index.js
+++ index.js
@@ -40,12 +40,14 @@ app.post("/performList", async (req, res) =>{
   let trigger_item = req.body.trigger_item;
   const nexus_client = new NexusClient();
   const nexus_response = await nexus_client.getDriver(trigger_key, "staging")
-  console.log("Get the driver response", nexus_response)
   let object = {};
   if(nexus_response){
     let selected_trigger_method = nexus_response.triggers.filter((trigger) => trigger.key === trigger_item);
+    console.log("filtered trigger: ", selected_trigger_method);
     if(selected_trigger_method.length >= 1){
+      console.log("Sample object: ", selected_trigger_method[0].operation);
       object = selected_trigger_method[0].operation.sample;
+
       res.status(200).json({items: [object]});
     }else{
       res.status(200).json({items: []});
